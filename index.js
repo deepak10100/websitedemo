62a9bae2bc9f1e3bfffecca41882a1d823b7e510
deepak10100 <deepakmalakar98185@gmail.com>
2026-07-05 18:14:19 +0530

    updates

---
+++ index.js
@@ -0,0 +1,22 @@
+$('.hero-slider').owlCarousel({
+
+items:1,
+
+loop:true,
+
+nav:true,
+
+dots:false,
+
+autoplay:false,
+
+autoplayTimeout:5000,
+
+smartSpeed:1000,
+
+navText:[
+'<i class="fa-solid fa-arrow-left"></i>',
+'<i class="fa-solid fa-arrow-right"></i>'
+]
+
+});
