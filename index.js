13806c6d4e22af51a8dbf094c39d3e22431756ee
deepak10100 <deepakmalakar98185@gmail.com>
2026-07-19 23:47:23 +0530

    update

--- index.js
+++ index.js
@@ -20,3 +20,148 @@ navText:[
 ]
 
 });
+
+$('.property-slider').owlCarousel({
+
+    loop:true,
+
+    margin:20,
+
+    nav:true,
+
+    dots:true,
+
+    autoplay:false,
+
+    autoplayTimeout:3000,
+
+    smartSpeed:800,
+
+    navText:[
+        '<span>&larr;</span>',
+        '<span>&rarr;</span>'
+    ],
+
+    responsive:{
+
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        992:{
+            items:3
+        }
+
+    }
+
+});
+
+$('.client-feedback-slider').owlCarousel({
+
+    loop:true,
+
+    margin:30,
+
+    nav:false,
+
+    dots:true,
+
+    autoplay:true,
+
+    autoplayTimeout:3000,
+
+    autoplayHoverPause:true,
+
+    smartSpeed:800,
+
+    responsive:{
+
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        992:{
+            items:3
+        }
+
+    }
+
+});
+
+$('.feedback-slider').owlCarousel({
+
+    loop:true,
+    center:true,
+    margin:35,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:4000,
+    smartSpeed:800,
+
+    navText:[
+
+        '<i class="fa-solid fa-arrow-left"></i>',
+        '<i class="fa-solid fa-arrow-right"></i>'
+
+    ],
+
+    responsive:{
+
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        1200:{
+            items:3
+        }
+
+    }
+
+});
+
+$('.blog-slider').owlCarousel({
+
+    loop:true,
+    margin:35,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    smartSpeed:800,
+
+    navText:[
+
+'<i class="fa-solid fa-arrow-left"></i>',
+'<i class="fa-solid fa-arrow-right"></i>'
+
+    ],
+
+    responsive:{
+
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        1200:{
+            items:3
+        }
+
+    }
+
+});
